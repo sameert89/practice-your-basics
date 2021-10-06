48ece090344fd3d404699441473223b80d3c6992
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2021-10-06 20:42:21 +0530

    Quesn16 added

---
+++ cpp/Quesn16.cpp
@@ -0,0 +1,14 @@
+//16. Define a function that can accept two strings as input and concatenate them and then print it in console.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int32_t main() { 
+	
+	string str1, str2;
+
+	cin >> str1 >> str2;
+	cout << str1 + str2 << endl;
+
+	return 0;
+}
