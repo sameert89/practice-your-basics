face4ba2993ec1252e8493e0f042dafed1a30308
geekvinay <76990270+geekvinay@users.noreply.github.com>
2021-10-06 18:21:03 +0530

    Solved question 10 in C (#35)
    * Create quesn10.c
    
    * Created quesn10.c
    
    * Delete quesn10.c
    
    * Created quesn15.c
    
    * Delete quesn15.c
    
    one pr one file
    
    Co-authored-by: Atharva Kadlag <atharvakadlag@gmail.com>

---
+++ c/quesn10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[1000];
+    int i, alphabets = 0, digits = 0, specialcharacters = 0;
+
+    printf("Enter  the string : ");
+    gets(s);
+
+    for (i = 0; s[i]; i++)
+    {
+        if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+            alphabets++;
+        else if (s[i] >= 48 && s[i] <= 57)
+            digits++;
+    }
+    printf("Alphabets = %d \n", alphabets);
+    printf("Numbers = %d \n", digits);
+    return 0;
+}
+
